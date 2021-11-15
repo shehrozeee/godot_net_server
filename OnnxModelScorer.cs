802f4c1b22643362caa2af36154cb38ae6bce7ae
Shehroze Malik <shehroze@innidata.com>
2021-11-15 04:56:48 -0500

    making it easier to user

--- OnnxModelScorer.cs
+++ OnnxModelScorer.cs
@@ -13,7 +13,7 @@ namespace godot_net_server
     {
         private readonly string modelLocation;
         private readonly MLContext mlContext;
-
+        private ITransformer model;
 
         public OnnxModelScorer(string modelLocation, MLContext mlContext)
         {
@@ -26,18 +26,27 @@ namespace godot_net_server
             [VectorType(10)]
             [ColumnName("input.1")]
             public float[] Features { get; set; }
+            public ModelInput(IEnumerable<float> input)
+            {
+                Features = input.ToArray();
+            }
+            public static List<ModelInput> MakeInput(IEnumerable<float> input)
+            {
+                List<ModelInput> input_array = new List<ModelInput>();
+                input_array.Add(new ModelInput(input));
+                return input_array;
+            }
+
         }
 
         private ITransformer LoadModel(string modelLocation)
         {
-            Console.WriteLine("Read model");
-            Console.WriteLine($"Model location: {modelLocation}");
-            
+
             // Create IDataView from empty list to obtain input data schema
             var data = mlContext.Data.LoadFromEnumerable(new List<ModelInput>());
 
             // Define scoring pipeline
-            var pipeline = mlContext.Transforms.ApplyOnnxModel(modelFile: modelLocation, outputColumnNames: new[] { "31","34" }, inputColumnNames: new[] { "input.1" });
+            var pipeline = mlContext.Transforms.ApplyOnnxModel(modelFile: modelLocation, outputColumnNames: new[] { "31", "34" }, inputColumnNames: new[] { "input.1" });
 
             // Fit scoring pipeline
             var model = pipeline.Fit(data);
@@ -48,16 +57,13 @@ namespace godot_net_server
         {
             [VectorType(6)]
             [ColumnName("31")]
-            public float[] action{ get; set; }
+            public float[] action { get; set; }
             [VectorType(1)]
             [ColumnName("34")]
-            public float[]  state { get; set; }
+            public float[] state { get; set; }
         }
         private IEnumerable<float> PredictDataUsingModel(IDataView testData, ITransformer model)
         {
-            Console.WriteLine("");
-            Console.WriteLine("=====Identify the objects in the images=====");
-            Console.WriteLine("");
 
             IDataView scoredData = model.Transform(testData);
 
@@ -67,9 +73,19 @@ namespace godot_net_server
             return a[0];
         }
 
-        public IEnumerable<float> Score(IDataView data)
+        public IEnumerable<float> Score(ModelInput input)
         {
-            var model = LoadModel(modelLocation);
+            var data = mlContext.Data.LoadFromEnumerable(new[] { input });
+            if (model == null)
+                model = LoadModel(modelLocation);
+
+            return PredictDataUsingModel(data, model);
+        }
+        public IEnumerable<float> Score(IEnumerable<float> input)
+        {
+            var data = mlContext.Data.LoadFromEnumerable(new[] { new ModelInput(input) });
+            if (model == null)
+                model = LoadModel(modelLocation);
 
             return PredictDataUsingModel(data, model);
         }
--- Program.cs
+++ Program.cs
@@ -1,8 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
+using System.Linq;
 using Microsoft.ML;
-using Microsoft.ML.Data;
-using static godot_net_server.OnnxModelScorer;
 
 namespace godot_net_server
 {
@@ -10,31 +8,17 @@ namespace godot_net_server
     {
         static void Main(string[] args)
         {
-
-
-            string projectPath = "C:/Users/Shehroze/source/repos/GymGodot/gym-godot";//  # project.godot folder
-            string godotPath = "E:/godot/Godot/Godot_v3.4-stable_mono_win64.exe";//  # godot editor executable
-            string scenePath = "./examples/mars_lander/Root.tscn";//  # env Godot scene
-            string exeCmd = "cd {} && {} {}'.format(projectPath, godotPath, scenePath)";
-
-
             MLContext mlContext = new MLContext();
 
             // Load trained model
-
             var modelScorer = new OnnxModelScorer("my_ppo_1_model.onnx", mlContext);
-            List<ModelInput> input = new List<ModelInput>();
-            input.Add(new ModelInput()
+            var input = new[]
             {
-                Features = new[]
-                {
-                    3.036393f,11.0f,2.958097f,0.0f,0.0f,0.0f,0.015607f,0.684984f,0.0f,0.0f
-                }
-            });
-            var action = modelScorer.Score(mlContext.Data.LoadFromEnumerable(input));
-            action.ToString();
-            //ITransformer trainedModel = mlContext.Model.Load("my_ppo_1_model.onnx", out modelSchema);
-            //trainedModel.ToString();
+                3.036393f,11.0f,2.958097f,0.0f,0.0f,0.0f,0.015607f,0.684984f,0.0f,0.0f
+            };
+            var action = modelScorer.Score(input);
+            var action_command = action.ToList().IndexOf(action.ToList().Max());
+            Console.Write(action_command);
         }
         
     }
